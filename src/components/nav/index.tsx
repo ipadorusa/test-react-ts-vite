c681895acc5589ebe38aac683ccf8cf8c34cc8ad
ipadorusa <ipadorusa@gmail.com>
2023-12-25 22:47:48 +0900

    chore: sass loader + tspath

--- src/components/nav/index.tsx
+++ src/components/nav/index.tsx
@@ -1,10 +1,10 @@
 import { Link } from "react-router-dom"
-import "./indes.css";
+import "./indes.scss";
 
 export default function Nav() {
   return (
-    <nav className="menu">
-      <ul>
+    <nav className="nav">
+      <ul className="nav__list">
 				<Link to="/contact"><li>contact</li></Link>
 				<Link to="/list"><li>list</li></Link>
         <Link to="/about"><li>about</li></Link>
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,15 +1,11 @@
-import { Link } from "react-router-dom"
+import Nav from "@/components/nav"
 
 export default function HomePage() {
   return (
     <div>
       <h2>HomePage</h2>
       <div>Homepage Blah</div>
-      <ul>
-				<Link to="/contact"><li>contact</li></Link>
-				<Link to="/list"><li>list</li></Link>
-        <Link to="/about"><li>about</li></Link>
-			</ul>
+      <Nav />
     </div>
   )
 }
