7876ef13d9871d44c49d0ee3255c5ded7039a2dc
ipadorusa <ipadorusa@gmail.com>
2023-12-26 14:33:45 +0900

    fix: vite.config.js 내 scss 호출 부분 추, 린트 에러 수정

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,38 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
-import tsconfigPaths from 'vite-tsconfig-paths';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+const stylesPath = resolve(__dirname, "src/styles");
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   return {
     define: {
-      'process.env.NODE_ENV': JSON.stringify(mode),
+      "process.env.NODE_ENV": JSON.stringify(mode),
     },
     plugins: [react(), tsconfigPaths()],
     build: {
       outDir: "../../../webroot/mobile/js/calendar/dist",
       lib: {
         name: "calendar",
-        entry: resolve(__dirname, 'src/app.tsx'),
+        entry: resolve(__dirname, "src/app.tsx"),
         formats: ["es"],
         fileName: "app",
       },
       minify: "esbuild",
     },
-  }
-})
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `
+            @use "sass:math";
+            @import "${stylesPath}/abstracts/_functions.scss";
+            @import "${stylesPath}/abstracts/_mixins.scss";
+            @import "${stylesPath}/abstracts/_variables.scss";
+          `,
+        },
+      },
+    },
+  };
+});
